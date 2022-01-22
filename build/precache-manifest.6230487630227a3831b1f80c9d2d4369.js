616d57c165aa05b0e4cf5772bd21be7b04de707e
Joshua Soave <joshua.soave@Gmail.com>
2022-01-22 15:40:12 -0500

    removing  broken favicon

---
+++ build/precache-manifest.6230487630227a3831b1f80c9d2d4369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4617f777e4727b3fdffd830f03b282",
+    "url": "/fortune_teller/index.html"
+  },
+  {
+    "revision": "555a6eae46dbbc3df02d",
+    "url": "/fortune_teller/static/css/main.fc3af2d9.chunk.css"
+  },
+  {
+    "revision": "138c493cd0eaa63c35b8",
+    "url": "/fortune_teller/static/js/2.65b93b04.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/fortune_teller/static/js/2.65b93b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555a6eae46dbbc3df02d",
+    "url": "/fortune_teller/static/js/main.1426db16.chunk.js"
+  },
+  {
+    "revision": "b3d053d8138bb673a0c9",
+    "url": "/fortune_teller/static/js/runtime-main.cf46a3e6.js"
+  }
+]);
